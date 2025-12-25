2a7d0dea01c7559b1d67622f6981a7233b2840cb
Divine-Empire <mis@thedivineempire.com>
2025-12-25 15:29:14 +0530

    Update page.jsx

--- app/purchase/page.jsx
+++ app/purchase/page.jsx
@@ -793,7 +793,16 @@ export default function PurchasePage() {
       const form = formData[id] || {};
 
       return {
-        timestamp: new Date().toISOString(),
+        timestamp:  (() => {
+          const date = new Date();
+          const day = date.getDate().toString().padStart(2, "0");
+          const month = (date.getMonth() + 1).toString().padStart(2, "0");
+          const year = date.getFullYear();
+          const hours = date.getHours().toString().padStart(2, "0");
+          const minutes = date.getMinutes().toString().padStart(2, "0");
+          const seconds = date.getSeconds().toString().padStart(2, "0");
+          return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+        })(),
         vendorName: row.vendorName,
         indentNo: row.indentNo,
         poNo: row.referenceNo, // or voucherNo - jo PO No. ho
